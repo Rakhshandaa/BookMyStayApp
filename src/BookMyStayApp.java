03c4aa696c5d6f5617f6fc2134955001330836e2
rakhshandaa <rakhshandayadav@gmail.com>
2026-03-20 09:28:47 +0530

    Application Entry & Welcome Message

---
+++ src/BookMyStayApp.java
@@ -0,0 +1,24 @@
+/**
+ * Hotel Booking Application
+ * Entry point of the Book My Stay system
+ *
+ * @author Pari
+ * @version 1.0
+ */
+public class BookMyStayApp {
+
+    /**
+     * Main method - entry point of the program
+     */
+    public static void main(String[] args) {
+
+        // Welcome message
+        System.out.println("Welcome to Book My Stay App");
+
+        // Application details
+        System.out.println("Hotel Booking System v1.0");
+
+        // Status message
+        System.out.println("Application started successfully.");
+    }
+}
